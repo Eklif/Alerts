ee33f1cc1a7f0656ef5ef2b4db380caa2a7a9c48
Eklif <eklif89@gmail.com>
2025-04-30 10:40:44 +1000

    Update app.py

--- app.py
+++ app.py
@@ -106,4 +106,3 @@ def send_page():
 if __name__ == '__main__':
     port = int(os.environ.get("PORT", 5001))
     socketio.run(app, host='0.0.0.0', port=port, debug=False)
-не сработала очередь сообщений
